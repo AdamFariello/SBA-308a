658c338ec91142af2b8804db4bdc8b64240dc089
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-08-26 22:50:28 -0400

    Changing plan to using quote apis

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const baseUrl = "https://musicbrainz.org/ws/2/";
-const jsonArg = "&fmt=json"
+const baseUrl = "https://api.quotable.io";
 
-async function example() {
-    let url = baseUrl + "area/45f07934-675a-46d6-a577-6f8637a411b1?inc=aliases";
-    url += jsonArg;
-    
+
+async function randomQuote() {
+    const url = "https://api.quotable.io/quotes/random"
+
     const response = await fetch(url);
-    //const data = response.json();
-    //console.log(data);
-    console.log(response)
-
     let result = await response.json();
-    console.log(result)
-    console.log(JSON.stringify(result, null, "\t"))
+    //console.log(result)
+    //console.log(JSON.stringify(result, null, "\t"))
+
+    let quote = result[0].content;
+    console.log(quote)
+
+    //let author = result[0].author;
+    //console.log(author)
 }
-example();
-
-
+randomQuote();
